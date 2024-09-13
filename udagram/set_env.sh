917259b85a44ed902dc2806bbe78f46296f67553
CharlesWilkenson <charleswilkenson13@gmail.com>
2024-09-13 15:39:18 +0700

    First commit

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=udagramdb.c4wf1rash89b.us-west-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
-export AWS_REGION=us-east-1
+export AWS_BUCKET=arn:aws:s3:::mybucket033212455159
+export AWS_REGION=us-west-2
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
 export URL=http://localhost:8100
