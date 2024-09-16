7f317ebb2bc72a8a5a48cf563b2a95c4d00b757e
CharlesWilkenson <charleswilkenson13@gmail.com>
2024-09-16 16:52:17 +0700

    update config,ts file

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -7,15 +7,15 @@ dotenv.config();
 // Are Also needed
 
 export const config = {
-  username: `${process.env.POSTGRES_USERNAME}`,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  host: process.env.POSTGRES_HOST,
-  aws_region: process.env.AWS_REGION,
-  aws_profile: process.env.AWS_PROFILE,
-  aws_media_bucket: process.env.AWS_BUCKET,
+  username: `${process.env.POSTGRES_USERNAME}` || 'postgres',
+  password: process.env.POSTGRES_PASSWORD ||'myPassword',
+  database: process.env.POSTGRES_DB || 'postgres',
+  host: process.env.POSTGRES_HOST || 'udagramdb.c4wf1rash89b.us-west-2.rds.amazonaws.com',
+  aws_region: process.env.AWS_REGION || 'us-west-2',
+  aws_profile: process.env.AWS_PROFILE || 'default',
+  aws_media_bucket: process.env.AWS_BUCKET || 'arn:aws:s3:::mybucket033212455159',
   url: process.env.URL,
   jwt: {
-    secret: process.env.JWT_SECRET,
+    secret: process.env.JWT_SECRET || 'mysecretstring',
   },
 };
