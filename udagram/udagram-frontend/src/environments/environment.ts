c661312d61f653d727a9b90145a824af272656ac
CharlesWilkenson <charleswilkenson13@gmail.com>
2024-09-17 12:33:55 +0700

    Fix issues

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'udagram-api-dev.us-west-2.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
