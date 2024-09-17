4b62dcb3ae60703317ab6dd41d0f5dc1ed6ee833
CharlesWilkenson <charleswilkenson13@gmail.com>
2024-09-17 14:27:16 +0700

    fix

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -2,7 +2,7 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=udagram-api-dev.us-west-2.elasticbeanstalk.com
+export POSTGRES_HOST=udagramdb.c4wf1rash89b.us-west-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::mybucket033212455159
 export AWS_REGION=us-west-2
