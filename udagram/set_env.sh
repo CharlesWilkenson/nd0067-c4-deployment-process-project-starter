f63ee8bc0b8234646f186ab7e2d4292d1f53937a
Charles Wilkenson <32202574+CharlesWilkenson@users.noreply.github.com>
2024-09-17 09:41:48 +0700

    fix api package.json

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -2,12 +2,12 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=udagramdb.c4wf1rash89b.us-west-2.rds.amazonaws.com
+export POSTGRES_HOST=udagram-api-dev.us-west-2.elasticbeanstalk.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::mybucket033212455159
 export AWS_REGION=us-west-2
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=udagram-api-dev.us-west-2.elasticbeanstalk.com
+export URL=http://localhost:8080
 
 
