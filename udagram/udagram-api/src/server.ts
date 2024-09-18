2c028ca714e24dce7e4323a82e9cdb4a2dc3d530
Charles Wilkenson <32202574+CharlesWilkenson@users.noreply.github.com>
2024-09-18 16:19:18 +0700

    set preflightContinue to false

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -41,11 +41,12 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
       'Access-Control-Allow-Methods'
     ],
     "methods": 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    "preflightContinue": true,
+    "preflightContinue": false,
     "origin": '*',
   }));
 
   app.use("/api/v0/", IndexRouter);
+
 
   // Root URI call
   app.get("/", async (req, res) => {
